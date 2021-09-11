f2283a460d8e60c26e965e2e17cc70ffb24c0d85
shakedbo <shakedbozi@gmail.com>
2021-09-11 16:38:12 +0300

    Adding new files for tests

--- gatling-files/DevOpsProject.scala
+++ gatling-files/DevOpsProject.scala
@@ -49,9 +49,9 @@ class DevOpsProject extends Simulation {
 			//nothingFor(5.seconds), 
 			// rampUsers(10).during(10.seconds),
 			// rampUsersPerSec(1).to(30).during(15.seconds),
-			rampUsersPerSec(1).to(45).during(10.seconds),
-			nothingFor(40.seconds),
-			rampUsersPerSec(45).to(1).during(10.seconds)
+			rampUsersPerSec(1).to(45).during(20.seconds),
+			atOnceUsers(45)
+			rampUsersPerSec(45).to(1).during(40.seconds)
 			//rampUsersPerSec(45).to(1).during(30.seconds)
 			//heavisideUsers(45).during(60.seconds),
 			//rampUsers(45).during(60 seconds)
